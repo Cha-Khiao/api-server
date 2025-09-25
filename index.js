08eb0572fa6161ea974dddc3f68fa27fb94e1d74
Cha-Khiao <p.prasopphol203@gmail.com>
2025-09-26 02:53:23 +0700

    confix api

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${PORT}`,
+        url: `https://cut-match-api.onrender.com`,
         description: 'Development Server'
       },
     ],
@@ -74,5 +74,5 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
-  console.log(`📚 API Documentation available at http://localhost:${PORT}/api-docs`);
+  console.log(`📚 API Documentation available at https://cut-match-api.onrender.com/api-docs`);
 });
