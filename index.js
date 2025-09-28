f0b407bcedde9dd3fc8605561849a4430077f93f
Cha-Khiao <p.prasopphol203@gmail.com>
2025-09-28 23:28:50 +0700

    confix

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const rateLimit = require('express-rate-limit');
 // Import Routes
 const userRoutes = require('./routes/userRoutes.js');
 const hairstyleRoutes = require('./routes/hairstyleRoutes.js');
-const postRoutes = require('./routes/postRoutes.js');
 
 // Import Middleware
 const { errorHandler } = require('./middleware/errorMiddleware.js');
@@ -106,7 +105,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', userRoutes);
 app.use('/api/hairstyles', hairstyleRoutes);
-app.use('/api/posts', postRoutes);
 
 // --- Centralized Error Handler ---
 app.use(errorHandler);
